80bff828c3b17dc2667e03b2561ec04d39cd7a50
igorpieniek <pieniek97@gmail.com>
2020-10-22 09:57:12 +0200

    Update joy constructor and write convert adc values

--- Core/Inc/Joy.cpp
+++ Core/Inc/Joy.cpp
@@ -9,18 +9,29 @@
 
 
 
-Joy::Joy(float max, float min, float neutral): v_max(max), v_min(min), v_neutral(neutral) {
-	// TODO Auto-generated constructor stub
+Joy::Joy(float max, float min, float neutral, uint8_t res, float ref):
+		v_max(max), v_min(min), v_neutral(neutral),
+		bit_resolution(res), v_ref(ref)
+{
 	v_max_normalized = v_max - v_neutral;
 	v_min_normalized = v_min - v_neutral;
+	max_number_levels = pow(2, bit_resolution);
 }
 
 Joy::~Joy() {
 	// TODO Auto-generated destructor stub
 }
 
+void Joy::calculate_data(uint32_t raw){
+	convert_adc_value(raw);
 
-void Joy::convert_adc_values(void){
+}
+
+
+
+void Joy::convert_adc_value(uint32_t raw){
+	raw_data = raw;
+	converted_data = ( (float)raw_data * v_ref ) / (float)max_number_levels;
 
 }
 void Joy::calculate_joy_data(void){
--- Core/Inc/Joy.h
+++ Core/Inc/Joy.h
@@ -9,17 +9,18 @@
 #define INC_JOY_H_
 
 #include "main.h"
+#include "math.h"
 
 
 class Joy {
 
 
 public:
-	Joy(float max, float min, float neutral);
+	Joy(float max, float min, float neutral, uint8_t res, float ref);
 	virtual ~Joy();
 
 
-	void calculate_data();
+	void calculate_data(uint32_t raw);
 private:
 	uint32_t raw_data;
 	float v_max_normalized;
@@ -28,11 +29,15 @@ private:
 	float percentage_value;
 	uint8_t  sign;
 
+	uint32_t max_number_levels;
+
 	float v_max;
 	float v_min;
 	float v_neutral;
+	uint8_t bit_resolution;
+	float v_ref;
 
-	void convert_adc_values(void);
+	void convert_adc_value(uint32_t raw);
 	void calculate_joy_data(void);
 	uint8_t is_joy_near_neautral();
 
